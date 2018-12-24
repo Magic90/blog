d73dc772e1b0842983a986f3afef8294a5144dd7
Magic <1023607780@qq.com>
2018-12-24 21:56:31 +0800

    '更新代码'

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ const app = express()
 
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'))
-// 设置模板引擎为 ejs
+// 设置模板引擎为ejs
 app.set('view engine', 'ejs')
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')))
+
 // session 中间件
 app.use(session({
-  name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
-  secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
-  resave: true, // 强制更新 session
-  saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+  name: config.session.key, // 设置cookie中保存session id的字段名称
+  secret: config.session.secret, // 通过设置secret 来计算hash值并放在cookie中，使产生的signedCookie 防篡改
+  resave: true, // 强制更新session
+  saveUninitialized: false, // 设置为false，强制创建一个session，即使用户未登录
   cookie: {
-    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+    maxAge: config.session.maxAge // 设置过期时间，过期后cookie中的session id自动删除
   },
-  store: new MongoStore({// 将 session 存储到 mongodb
-    url: config.mongodb// mongodb 地址
+  store: new MongoStore({ // 将session存储到MongoDB
+    url: config.mongodb // mongodb 地址
   })
 }))
-// flash 中间件，用来显示通知
+
+// flash中间件，用来显示通知
 app.use(flash())
 
 app.locals.blog = {
@@ -37,17 +39,17 @@ app.locals.blog = {
   description: pkg.description
 }
 
-// 添加模板必需的三个变量
 app.use(function (req, res, next) {
+  console.log(`req----------------------`, req)
+  // console.log(`res---------------------`, res)
   res.locals.user = req.session.user
   res.locals.success = req.flash('success').toString()
   res.locals.error = req.flash('error').toString()
   next()
 })
-// 路由
+
 routes(app)
 
-// 监听端口，启动程序
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
 })
